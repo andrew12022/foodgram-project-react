5f3198c9d08c6786e034f67f25c40bb4a9d29b00
Андрей Елистратов <elistratov2001.2017@yandex.ru>
2023-11-25 19:38:15 +0400

    Add models users

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,1 +1,75 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.password_validation import validate_password
+from django.core.validators import RegexValidator
 from django.db import models
+
+
+class User(AbstractUser):
+    """Модель Пользователь."""
+    email = models.EmailField(
+        max_length=254,
+        verbose_name='Адрес электронной почты',
+        unique=True,
+    )
+    username = models.CharField(
+        max_length=150,
+        verbose_name='Уникальный юзернейм',
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^[\w.@+-]+$',
+                message='Ошибка! Проверьте вводимый формат'
+            )
+        ]
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя',
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия',
+    )
+    password = models.CharField(
+        max_length=150,
+        verbose_name='Пароль',
+        validators=[validate_password]
+    )
+
+    class Meta:
+        verbose_name = 'объект "Пользователь"'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self) -> str:
+        return self.username
+
+
+class Subscriber(models.Model):
+    """Модель Подписчик."""
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Подписчик',
+        related_name='user_subscribers',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+        related_name='author_subscribers',
+    )
+
+    class Meta:
+        verbose_name = 'объект "Подписчик"'
+        verbose_name_plural = 'Подписчики'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_subscriber'
+            )
+        ]
+
+    def __str__(self) -> str:
+        return (
+            f'{self.user} подписался на {self.author}'
+        )
