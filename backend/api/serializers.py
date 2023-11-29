44a319390c642b894779966f4058dc2e4f991ecc
Андрей Елистратов <elistratov2001.2017@yandex.ru>
2023-11-30 02:08:44 +0400

    Add new urls

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,11 +1,26 @@
+import base64
+
+import webcolors
 from django.contrib.auth import get_user_model
+from django.core.files.base import ContentFile
 from django.db.models import F
-# from drf_extra_fields.fields import Base64ImageField
 from djoser.serializers import UserCreateSerializer, UserSerializer
-from recipes.models import Ingredient, Tag, Recipe
+from recipes.models import Favorite, Ingredient, Recipe, ShoppingCart, Tag
 from rest_framework import serializers
 
 User = get_user_model()
+
+
+class Hex2NameColor(serializers.Field):
+    def to_representation(self, value):
+        return value
+
+    def to_internal_value(self, data):
+        try:
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        return data
 
 
 class UserCreateSerializer(UserCreateSerializer):
@@ -42,7 +57,48 @@ class UserSerializer(UserSerializer):
         return user.user_subscribers.filter(author=object).exists()
 
 
+class SubscribeSerializer(UserSerializer):
+    recipes_count = serializers.SerializerMethodField()
+    recipes = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed',
+            'recipes',
+            'recipes_count',
+        )
+        read_only_fields = (
+            'email',
+            'username',
+            'first_name',
+            'last_name',
+        )
+
+    def get_recipes_count(self, object):
+        return object.recipes.count()
+
+    def get_recipes(self, object):
+        request = self.context.get('request')
+        limit = request.GET.get('recipes_limit')
+        recipes = object.recipes.all()
+        if limit:
+            recipes = recipes[:int(limit)]
+        serializer = RecipeShortSerializer(
+            recipes,
+            many=True,
+            read_only=True,
+        )
+        return serializer.data
+
+
 class TagSerializer(serializers.ModelSerializer):
+    color = Hex2NameColor()
 
     class Meta:
         model = Tag
@@ -65,6 +121,15 @@ class IngredientSerializer(serializers.ModelSerializer):
         )
 
 
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+        return super().to_internal_value(data)
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(
         many=True,
@@ -76,6 +141,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     ingredients = serializers.SerializerMethodField()
     is_favorited = serializers.SerializerMethodField()
     is_in_shopping_cart = serializers.SerializerMethodField()
+    image = Base64ImageField()
 
     class Meta:
         model = Recipe
@@ -112,3 +178,46 @@ class RecipeSerializer(serializers.ModelSerializer):
         if user.is_anonymous:
             return False
         return user.shopping_carts.filter(recipe=object).exists()
+
+
+class RecipeShortSerializer(serializers.ModelSerializer):
+    image = Base64ImageField()
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id',
+            'name',
+            'image',
+            'cooking_time',
+        )
+
+
+class FavoriteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Favorite
+        fields = (
+            'user',
+            'recipe',
+        )
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data = RecipeShortSerializer(instance.recipe).data
+        return data
+
+
+class ShoppingCartSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ShoppingCart
+        fields = (
+            'user',
+            'recipe',
+        )
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data = RecipeShortSerializer(instance.recipe).data
+        return data
